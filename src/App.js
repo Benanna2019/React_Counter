bdee7ae8bf1fc2618b91b00b9f8804fef56629cb
Benanna2019 <bass41992ben@gmail.com>
2020-09-29 08:53:17 -0400

    implemented useContext on counter app to toggle state.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import Counter from "./components/Counter";
 
+export const ThemeContext = React.createContext();
+
 function App() {
+  const [theme, setTheme] = useState("red");
   return (
-    <>
+    <ThemeContext.Provider value={{ backgroundColor: theme }}>
       <Counter initialCount={0} />
-    </>
+      <button
+        onClick={() =>
+          setTheme((prevTheme) => {
+            return prevTheme === "red" ? "blue" : "red";
+          })
+        }
+      >
+        ToggleTheme
+      </button>
+    </ThemeContext.Provider>
   );
 }
 
